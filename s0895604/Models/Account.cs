3ef490c77f8258027b8504df8b275ceb494ea7bc
Bart Langelaan <langelaanbart@gmail.com>
2015-11-04 13:49:23 +0100

    Update Account Model

--- s0895604/Models/Account.cs
+++ s0895604/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -11,18 +12,45 @@ namespace s0895604.Models
         [Key]
         public int Id { get; set; }
 
+        [Required]
+        [DisplayName("Gebruikersnaam")]
         public string Username { get; set; }
 
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Wachtwoord")]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Voornaam")]
         public string FirstName { get; set; }
 
+        [Required]
+        [DisplayName("Achternaam")]
         public string LastName { get; set; }
 
-        public AccountStatus Status { get; set; }
+        [DisplayName("Rol")]
+        public AccountRole Role { get; set; }
 
+        [DisplayName("Actief")]
         public bool Active { get; set; }
+
+        public Account()
+        {
+            Role = AccountRole.User;
+            Active = true;
+        }
+
+        private static Account _activeAccount;
+
+        public static Account ActiveAccount
+        {
+            get { return _activeAccount }
+            set { _activeAccount = value; }
+        }
     }
 
-    public enum AccountStatus
+    public enum AccountRole
     {
         Admin = 1,
         User = 2
