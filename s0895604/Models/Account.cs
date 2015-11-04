3cfae0063fc2f60d124cc7da272778565624b044
Bart Langelaan <langelaanbart@gmail.com>
2015-11-04 12:09:43 +0100

    Create AccountModel

---
+++ s0895604/Models/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace s0895604.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public AccountStatus Status { get; set; }
+
+        public bool Active { get; set; }
+    }
+
+    public enum AccountStatus
+    {
+        Admin = 1,
+        User = 2
+    }
+}
