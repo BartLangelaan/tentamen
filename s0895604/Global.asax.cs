1ab206f6c6bd22d8fb0668db3657fa3a3a8967a0
Bart Langelaan <langelaanbart@gmail.com>
2015-11-04 12:23:11 +0100

    Add temporary Database fix

--- s0895604/Global.asax.cs
+++ s0895604/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using s0895604.Models;
 
 namespace s0895604
 {
@@ -12,6 +14,7 @@ namespace s0895604
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new DropCreateDatabaseAlways<DatabaseContext>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
